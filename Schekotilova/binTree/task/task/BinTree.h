058f8a6caf7110f084d013654c407d89f694ae60
shekotilova-yuliya <43138513+shekotilova-yuliya@users.noreply.github.com>
2020-05-18 23:11:38 +0300

    Update BinTree.h

--- Schekotilova/binTree/task/task/BinTree.h
+++ Schekotilova/binTree/task/task/BinTree.h
@@ -3,22 +3,18 @@
 #include"treenode.h"
 #include <time.h>
 
-using namespace std;
-
 class BinTree
 {
+	treeNode* root;
 public:
-	Node* root;
-
-	Node();
-	Node(int);
-	~Node() { left = nullptr; right = nullptr; }
-	static Node* search(Node*, int);
-	Node* searchMin(Node*);
-	Node* searchNxt(Node*);	
-	static void insertNode(Node*, Node*);
-	void deleteNode(Node*);
-	static void print(const Node*);
+	BinTree(treeNode* rt = nullptr) : root(rt) {};
+	treeNode* getRoot() const;
+	static treeNode* search(treeNode*, int);
+	treeNode* searchMin(treeNode*);
+	treeNode* searchNxt(treeNode*);	
+	static void insertNode(treeNode*, treeNode*);
+	void deleteNode(treeNode*);
+	static void print(const treeNode*);
 	friend ostream& operator<<(ostream& out, const BinTree& btree)
 	{
 		if (btree.root != nullptr) BinTree::print(btree.root);
